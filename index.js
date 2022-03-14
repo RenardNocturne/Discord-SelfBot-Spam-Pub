95164e5c46d9b8429c69ea88ec83feb4b24c0568
Renard Nocturne <RenardNocturne@outlook.com>
2022-03-14 11:10:54 +0100

    refactor: :truck: Changed paths.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 const { Client, Intents } = require('discord.js-selfbot');
-const { loadEvents } = require('./Utils/loader')
+const { loadEvents } = require('./Utils/loader.js')
 
 const userClient = new Client();
 const client = new Discord.Client()
 
 require('./Utils/functions.js')(userClient);
-userClient.config = require('./Ignore/config')
+userClient.config = require('./config.js')
 loadEvents(userClient);
 
 userClient.login(userClient.config.TOKEN)
