3faeaf80ed9dc599e2140981ed152ddfa9cf2941
Renard Nocturne <RenardNocturne@outlook.com>
2022-03-14 11:40:46 +0100

    refactor: :bulb: Added some instructions...

--- Events/Ads/spamAds.js
+++ Events/Ads/spamAds.js
@@ -5,4 +5,6 @@ module.exports = async (client, item, pickRandomNumberBetweenTwoNumbers) => {
         .then(() => console.log(`✔ Sent ad to ${channel.name} !`))
         .catch(err => console.log(`❌ I can't send the ad to ${channel.name}:`, err))
     }, item.slowModeInMs + pickRandomNumberBetweenTwoNumbers(30, 60)) //add it some minutes to act like a human
+    //You can change the values like: pickRandomNumberBetweenTwoNumbers(15, 30) and it will send your ad more often
+    // pickRandomNumberBetweenTwoNumbers(<min minutes to wait after the slowmode ends>, <max minutes to wait after the slowmode ends>)
 }
