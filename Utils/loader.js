76f1165f7e1bd5998fef7d7e82b0c9d63d2b1e9a
Renard Nocturne <RenardNocturne@outlook.com>
2022-03-15 15:08:39 +0100

    feat: :sparkles: Added loadData function.

--- Utils/loader.js
+++ Utils/loader.js
@@ -1,4 +1,5 @@
-const { readdirSync } = require('fs');
+const { readdirSync, writeFile } = require('fs');
+const inquirer = require("inquirer")
 
 const loadEvents = (client, dir = './Events') => {
     readdirSync(dir).forEach(dirs => {
@@ -14,6 +15,40 @@ const loadEvents = (client, dir = './Events') => {
     });
 };
 
+const loadData = async () => {
+    inquirer.prompt([
+        {
+            type: 'password',
+            name: 'TOKEN',
+            message: "Please, enter the Discord account token:",
+            mask: '*'
+        },
+        {
+            type: 'input',
+            name: 'ADMESSAGE',
+            message: 'Please, enter your publicity, you can change it later in the data.json file located in the Data folder:',
+        },
+        {
+            type: 'input',
+            name: 'CHANNELS',
+            message: "Please, enter the channels IDs: <Channel ID> <Another channel ID>",
+        }
+    ])
+    .then(res => {
+        const data = {
+            TOKEN: res.TOKEN,
+            ADMESSAGE: res.ADMESSAGE,
+            CHANNELS: res.CHANNELS.split(" ")  
+        }
+        dataString = `module.exports = ${JSON.stringify(data)}`
+        writeFile("./config.js", dataString, ((err, result) => {
+            console.log("✅ I've successfully created your data ! You can run node .\\index.js or manually modify these values in the config.js file !");    
+        }))
+    })
+    .catch(err => console.log(err + "❌ An error has occurred !"))
+}
+
 module.exports = {
     loadEvents,
+    loadData
 }
